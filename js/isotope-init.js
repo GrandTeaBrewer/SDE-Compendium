9f87fb96a0d86b2db86428ad4e614c32f1993b7d
Invarti <topher.newon@gmail.com>
2016-08-13 15:55:39 +1000

    Commit new version
    Updated filters to checkboxes, simplified layout to a single Isotope
    layout.

--- js/isotope-init.js
+++ js/isotope-init.js
@@ -1,221 +1,222 @@
-// sectionFilter controls
+$(function() {
 
-$( document ).ready(function() {
-	// init Isotope
-	var $grid = $('#sectionFilter').isotope({
-		itemSelector: '.sectionFilter',
-		layoutMode: 'fitRows'
-	});
-	// filter functions
-	var filterFns = {
-		// show if number is greater than 50
-		numberGreaterThan50: function() {
-			var number = $(this).find('.number').text();
-			return parseInt( number, 10 ) > 50;
-		},
-		// show if name ends with -ium
-		ium: function() {
-			var name = $(this).find('.name').text();
-			return name.match( /ium$/ );
-		}
-	};
-	// bind filter on select change
-	$('.filters-select').on( 'change', function() {
-		// get filter value from option value
-		var filterValue = this.value;
-		// use filterFn if matches value
-		filterValue = filterFns[ filterValue ] || filterValue;
-		$grid.isotope({ filter: filterValue });
-		
-		// ===================================================
-		// init Isotope on second container
-		
-		
-		$( function() {
-			// quick search regex
-			var qsRegex;
-			var buttonFilter;
-			
-			// init Isotope
-			var $container = $('.charFilter').isotope({
-				itemSelector: '.charProfile',
-				layoutMode: 'fitRows',
-				filter: function() {
-					console.log('filtering');
-					var $this = $(this);
-					var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
-					var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
-					return searchResult && buttonResult;
+		//  All filter option controls
+		$("input:checkbox.all").change(function() {
+        if ($('input[name=charFilterAll]:checked').length) {
+            $('input[name=charFilter]').removeAttr('checked');
+        }
+				if ($('input[name=retailFilterAll]:checked').length) {
+						$('input[name=retailFilter]').removeAttr('checked');
 				}
-			});
-			
-			// store filter for each group
-			var filters = {};
-			
-			
-			$('#filters').on( 'click', '.button', function() {
-				var $this = $(this);
-				// get group key
-				var $buttonGroup = $this.parents('.button-group');
-				var filterGroup = $buttonGroup.attr('data-filter-group');
-				// set filter for group
-				filters[ filterGroup ] = $this.attr('data-filter');
-				// combine filters
-				var filterValue = '';
-				for ( var prop in filters ) {
-					filterValue += filters[ prop ];
+				if ($('input[name=packageFilterAll]:checked').length) {
+						$('input[name=packageFilter]').removeAttr('checked');
 				}
-				buttonFilter = filterValue;
-				// set filter for Isotope
-				$container.isotope();	
-				
-			});
-			
-			// use value of search field to filter
-			var $quicksearch = $('#quicksearch').keyup( debounce( function() {
-				qsRegex = new RegExp( $quicksearch.val(), 'gi' );
-				$container.isotope();
-			}) );
-			
-			
-			// change is-checked class on buttons
-			$('.button-group').each( function( i, buttonGroup ) {
-				var $buttonGroup = $( buttonGroup );
-				$buttonGroup.on( 'click', 'button', function() {
-					$buttonGroup.find('.is-checked').removeClass('is-checked');
-					$( this ).addClass('is-checked');
-				});
-			});
-			
-		});
-		
-		// debounce so filtering doesn't happen every millisecond
-		function debounce( fn, threshold ) {
-			var timeout;
-			return function debounced() {
-				if ( timeout ) {
-					clearTimeout( timeout );
-				}
-				function delayed() {
-					fn();
-					timeout = null;
-				}
-				setTimeout( delayed, threshold || 100 );
-			};
-		}
-		
-		// ===================================================
-		
-	});
-});	
+    });
 
-// charFilter controls
-$( document ).ready(function() {
-	
-	
-	$( function() {
-		// quick search regex
-		var qsRegex;
-		var buttonFilter;
-		
-		// init Isotope
-		var $container = $('.charFilter').isotope({
-			itemSelector: '.charProfile',
-			layoutMode: 'fitRows',
-			filter: function() {
-				console.log('filtering');
-				var $this = $(this);
-				var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
-				var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
-				return searchResult && buttonResult;
-			}
-		});
-		
-		// store filter for each group
-		var filters = {};
-		
-		
-		$('#filters').on( 'click', '.button', function() {
-			var $this = $(this);
-			// get group key
-			var $buttonGroup = $this.parents('.button-group');
-			var filterGroup = $buttonGroup.attr('data-filter-group');
-			// set filter for group
-			filters[ filterGroup ] = $this.attr('data-filter');
-			// combine filters
-			var filterValue = '';
-			for ( var prop in filters ) {
-				filterValue += filters[ prop ];
-			}
-			buttonFilter = filterValue;
-			// set filter for Isotope
-			$container.isotope();
-			
-			
-			// ===================================================
-			// init Isotope on main container
-			
-			var $grid = $('#sectionFilter').isotope({
-				itemSelector: '.sectionFilter',
-				layoutMode: 'fitRows'
-			});
-			// filter functions
-			var filterFns = {
-				// show if number is greater than 50
-				numberGreaterThan50: function() {
-					var number = $(this).find('.number').text();
-					return parseInt( number, 10 ) > 50;
-				},
-				// show if name ends with -ium
-				ium: function() {
-					var name = $(this).find('.name').text();
-					return name.match( /ium$/ );
-				}
-			};
-			// bind filter on select change
-			$('.filters-select').on( 'change', function() {
-				// get filter value from option value
-				var filterValue = this.value;
-				// use filterFn if matches value
-				filterValue = filterFns[ filterValue ] || filterValue;
-				$grid.isotope({ filter: filterValue });
-			});
-			
-			// ===================================================			
-			
-		});
-		
-		// use value of search field to filter
-		var $quicksearch = $('#quicksearch').keyup( debounce( function() {
-			qsRegex = new RegExp( $quicksearch.val(), 'gi' );
-			$container.isotope();
-		}) );
-		
-		
-		// change is-checked class on buttons
-		$('.button-group').each( function( i, buttonGroup ) {
-			var $buttonGroup = $( buttonGroup );
-			$buttonGroup.on( 'click', 'button', function() {
-				$buttonGroup.find('.is-checked').removeClass('is-checked');
-				$( this ).addClass('is-checked');
-			});
-		});
-		
-	});
-	
-	// debounce so filtering doesn't happen every millisecond
-	function debounce( fn, threshold ) {
-		var timeout;
-		return function debounced() {
-			if ( timeout ) {
-				clearTimeout( timeout );
-			}
-			function delayed() {
-				fn();
-				timeout = null;
-			}
-			setTimeout( delayed, threshold || 100 );
-		};
-	}
-	
-});		
+		// Any charFilter checked
+    $("input[name=charFilter]:checkbox").change(function() {
+        if ($('input[name=charFilter]:checked').length) {
+            $('input[name=charFilterAll]').prop("checked", false);
+        } else {
+            $('input[name=charFilterAll]').prop("checked", true);
+        }
+    });
+
+		// Any retailFilter checked
+    $("input[name=retailFilter]:checkbox").change(function() {
+        if ($('input[name=retailFilter]:checked').length) {
+            $('input[name=retailFilterAll]').prop("checked", false);
+        } else {
+            $('input[name=retailFilterAll]').prop("checked", true);
+        }
+    });
+
+		// Any packageFilter checked
+    $("input[name=packageFilter]:checkbox").change(function() {
+        if ($('input[name=packageFilter]:checked').length) {
+            $('input[name=packageFilterAll]').prop("checked", false);
+        } else {
+            $('input[name=packageFilterAll]').prop("checked", true);
+        }
+    });
+});
+
+var $container;
+var filters = {};
+
+$(function() {
+
+    var qsRange;
+    var qsRegex;
+    $container = $('.charFilter');
+
+    var $grid = $('.charFilter').isotope({
+        layoutMode: 'fitRows'
+    });
+
+    var $filterDisplay = $('#filter-display');
+
+    var comboFilter = getComboFilter(filters);
+
+    //preLoadHashFilters();
+    var hash = location.hash.slice(1),
+        hashParts = hash.split('&search:'),
+        hashFilters = [],
+        hashSearch = hashParts[1] || '';
+
+    hashParts[0]
+        .split(',')
+        .filter(function(item) {
+            return $.trim(item) !== '';
+        })
+        .forEach(function(item) {
+            var classes = $.trim(item).split('.').filter(function(item) {
+                return item !== '';
+            });
+            classes.forEach(function(item) {
+                if (hashFilters.indexOf(item) == -1) {
+                    hashFilters.push(item);
+                }
+            });
+        });
+
+    if (hashSearch !== '' || hashFilters.length) {
+        $('#options').find(':checkbox').filter(function(index, item) {
+            return hashFilters.indexOf(item.value.slice(1)) > -1;
+        }).prop('checked', true);
+    }
+
+    $container.isotope({
+        filter: function() {
+            var $this = $(this),
+                comboResult = $this.is(comboFilter),
+                searchResult = qsRange ? $(this).text().match(qsRange) : true;
+
+            return (comboResult || comboFilter === '') && searchResult;
+        }
+    });
+
+    // do stuff when checkbox change
+    var $options = $('#options').on('change', function(jQEvent) {
+        var $checkbox = $(jQEvent.target);
+        manageCheckbox($checkbox);
+        comboFilter = getComboFilter(filters);
+
+        $container.isotope();
+        setHashText(comboFilter, qsRange);
+    });
+
+    // use value of search field to filter
+    var $search = $('#search').keyup(debounce(
+
+        function() {
+            qsRange = new RegExp($search.val(), 'gi');
+            qsRange = qsRange;
+            $container.isotope();
+            setHashText(comboFilter, qsRange);
+        }));
+
+    if (hashFilters.length) {
+        $options.find(':checked:not([value=""])').trigger('change');
+    }
+    if (hashSearch !== '') {
+        $search.val(hashSearch).trigger('keyup');
+    }
+});
+
+
+function setHashText(comboFilter, range) {
+    var hash = comboFilter;
+    var actual = $('#search').val();
+
+    if (range) {
+        hash += '&search:' + actual;
+    }
+    location.hash = hash;
+    $('#filter-display').text(hash);
+}
+
+
+function getComboFilter(filters) {
+    var i = 0;
+    var comboFilters = [];
+    var message = [];
+
+    for (var prop in filters) {
+        message.push(filters[prop].join(' '));
+        var filterGroup = filters[prop];
+        // skip to next filter group if it doesn't have any values
+        if (!filterGroup.length) {
+            continue;
+        }
+        if (i === 0) {
+            // copy to new array
+            comboFilters = filterGroup.slice(0);
+        } else {
+            var filterSelectors = [];
+            // copy to fresh array
+            var groupCombo = comboFilters.slice(0); // [ A, B ]
+            // merge filter Groups
+            for (var k = 0, len3 = filterGroup.length; k < len3; k++) {
+                for (var j = 0, len2 = groupCombo.length; j < len2; j++) {
+                    filterSelectors.push(groupCombo[j] + filterGroup[k]); // [ 1, 2 ]
+                }
+
+            }
+            // apply filter selectors to combo filters for next group
+            comboFilters = filterSelectors;
+        }
+        i++;
+    }
+
+    var comboFilter = comboFilters.join(', ');
+    return comboFilter;
+}
+
+function manageCheckbox($checkbox) {
+    var checkbox = $checkbox[0];
+    var group = $checkbox.parents('.option-set').attr('data-group');
+
+    // create array for filter group, if not there yet
+    var filterGroup = filters[group];
+    if (!filterGroup) {
+        filterGroup = filters[group] = [];
+    }
+
+    var isAll = $checkbox.hasClass('all');
+
+    if (isAll) {
+        delete filters[group];
+    }
+    // index of
+    var index = $.inArray(checkbox.value, filterGroup);
+
+    if (checkbox.checked) {
+        var selector = isAll ? 'input' : 'input.all';
+
+        if (!isAll && index === -1) {
+            // add filter to group
+            filters[group].push(checkbox.value);
+        }
+
+    } else if (!isAll) {
+        // remove filter from group
+        filters[group].splice(index, 1);
+    }
+}
+
+// debounce so filtering doesn't happen every millisecond
+function debounce(fn, threshold) {
+    var timeout;
+    return function debounced() {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+
+        function delayed() {
+            fn();
+            timeout = null;
+        }
+        setTimeout(delayed, threshold || 100);
+    };
+}
